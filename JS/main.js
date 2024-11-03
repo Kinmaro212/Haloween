22b4a226ec5193eeabcdc4bf5d7f52453854de38
Kinmaro212 <toti98741@gmail.com>
2024-11-04 00:57:37 +0100

    projet Haloween

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+/*=============== SHOW MENU ===============*/
+const naMenu = document.getElementById('nav-menu'),
+ navToggle = document.getELementById(' nav-toggle'),
+  navclose = document.getElementById( 'nav-close') 
+  /* Menu show */ 
+if(navToggle) { 
+    navToggle. addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    }) 
+}
+/* Menu hidden */ 
+    if(navclose) { navClose. addEventListener('click', () =>{
+        navMenu. classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document. querySelectorALL('.nav__link')
+
+ const linkAction = () =>{ 
+    const navMenu = document.getElementById( 'nav-menu')
+
+navMenu. classList.remove(' show-menu')
+}
+navLink-forEach(n => n.addEventListener('click', LinkAction))
+
+
+/*=============== ATROPOS JS ===============*/
+// Initialize
+const myAtropos = Atropos({
+    el: '.home__images',
+    shadow: false,
+    highlight: false,
+
+  });
+
+/*=============== ADD SHADOW HEADER ===============*/
